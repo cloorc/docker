ab53231b7b93e0382459feef25dbcb9468826b4a
z0228 <zhangh@dtdream.com>
2016-12-05 16:25:52 +0800

    use origin tarball instead of self building one

--- mycat-server/start.sh
+++ mycat-server/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd $MYCAT_HOME/conf/
-sed -i "s/loadZk=false/loadZk=${E_LOAD_ZK:-false}/g" ./myid.properties
+sed -i "s/loadZk=false/loadZk=${E_ZK_LOAD:-false}/g" ./myid.properties
 sed -i "s/zkURL=127.0.0.1:2181/zkURL=${E_ZK_URL:-127.0.0.1:2181}/g" ./myid.properties
+cat ./myid.properties
 $MYCAT_HOME/bin/mycat $*
